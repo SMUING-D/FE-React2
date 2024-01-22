bdf55d74b98bf8f6d6a2898cd8365f93024efa95
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2024-01-22 15:12:03 +0900

    #14 Feat: footer 코드 리팩토링

--- smuing/src/components/navbar/Navbar.tsx
+++ smuing/src/components/navbar/Navbar.tsx
@@ -7,7 +7,7 @@ import useScroll from '../../hooks/useScroll'
 const Navbar: React.FC = () => {
   const { y } = useScroll()
   const navBackground = y < 100 ? 'bg-slate-900' : 'bg-transparent'
-  // navbar 내리면 투명화랑 햄버거 버튼 기능 아직 구현못함
+  // 햄버거 버튼 기능 아직 구현못함
   return (
     <nav
       className={`transition ease-in duration-500 w-screen h-[100px] fixed text-3xl text-white flex items-center p-5 justify-between ${navBackground}`}
