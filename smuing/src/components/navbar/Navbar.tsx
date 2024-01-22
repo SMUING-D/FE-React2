606e8ea594ef83d52412d2da712c5e979ec7d62e
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2024-01-22 16:02:24 +0900

    #14 Feat: 리덕스 파일및 세팅 추가

--- smuing/src/components/navbar/Navbar.tsx
+++ smuing/src/components/navbar/Navbar.tsx
@@ -1,13 +1,21 @@
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { MdOutlineDarkMode } from 'react-icons/md'
+import { useDispatch } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
+import { sidebarOpen } from '../../\bredux/slices/sidebarSlice'
+// 경로 왜이럼?
 import useScroll from '../../hooks/useScroll'
 
 const Navbar: React.FC = () => {
   const { y } = useScroll()
   const navBackground = y < 100 ? 'bg-slate-900' : 'bg-transparent'
   // 햄버거 버튼 기능 아직 구현못함
+  const dispatch = useDispatch()
+
+  const handleSidebar = () => {
+    dispatch(sidebarOpen())
+  }
   return (
     <nav
       className={`transition ease-in duration-500 w-screen h-[100px] fixed text-3xl text-white flex items-center p-5 justify-between ${navBackground}`}
@@ -20,8 +28,9 @@ const Navbar: React.FC = () => {
         <NavLink to="/notices">NOTICES</NavLink>
       </div>
       <div className="lg:hidden sm:gap-3 flex items-center">
+        {/* 햄버거 wrapper */}
         <MdOutlineDarkMode size={40} />
-        <button>
+        <button onClick={handleSidebar}>
           <GiHamburgerMenu />
         </button>
       </div>
--- smuing/src/main.tsx
+++ smuing/src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
+import { store } from './redux/store/store.ts'
+// 경로 왜이럼?
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
 )
