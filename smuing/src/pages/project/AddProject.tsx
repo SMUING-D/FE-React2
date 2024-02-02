4580590ea3455c55b2fef0aa0bf2396ce68ddda7
kimtaeyoung <trust00005@naver.com>
2024-02-03 04:41:17 +0900

    프로젝트 등록페이지 수정

--- smuing/src/pages/project/AddProject.tsx
+++ smuing/src/pages/project/AddProject.tsx
@@ -20,9 +20,6 @@ const AddProjectPage = () => {
     { id: 1, part: 'web', name: '김태영' },
     { id: 2, part: 'server', name: '임재영' },
     { id: 3, part: 'design', name: '디자이너' },
-    { id: 1, part: 'web', name: '김태영' },
-    { id: 2, part: 'server', name: '임재영' },
-    { id: 3, part: 'design', name: '디자이너' },
   ]
 
   const handleColaborator = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -63,15 +60,15 @@ const AddProjectPage = () => {
   const getWindowWidth = () => window.innerWidth
 
   return (
-    <>
+    <div className="bg-rgb-35-39-49">
       <LoginNav title="SMUMC for management team" />
-      <div className="bg-rgb-35-39-49 h-full w-full">
+      <div className="bg-rgb-35-39-49 h-full w-full min-h-screen">
         <div className="text-center pt-[80px]">
           <p className="text-white text-4xl pb-[10px]">프로젝트 등록</p>
           <p className="text-white text-sm">*운영진들만 접근 가능한 프로젝트 등록 페이지입니다.</p>
         </div>
 
-        <div className="text-center">
+        <div className="flex flex-col items-center mt-[20px]">
           <div className="w-full sm:w-[200px]">
             <button
               type="button"
@@ -81,7 +78,7 @@ const AddProjectPage = () => {
               인원 선택하기
             </button>
             {isDropdownOpen && (
-              <div className="fixed">
+              <div className="">
                 <select
                   value={formData.people}
                   onChange={handleColaborator}
@@ -97,7 +94,7 @@ const AddProjectPage = () => {
               </div>
             )}
           </div>
-          <div className="text-white ml-[-25%] mt-4">
+          <div className="text-white mt-4">
             <p>프로젝트 참여 인원:</p>
             <ul className="flex flex-wrap gap-2">
               {formData.people.map((id) => {
@@ -115,6 +112,7 @@ const AddProjectPage = () => {
             </ul>
           </div>
         </div>
+
         <div className="text-center pt-4 sm:pt-[10px]">
           {titleText.map((title, index) => (
             <TextArea
@@ -129,15 +127,17 @@ const AddProjectPage = () => {
             />
           ))}
         </div>
-        <button
-          type="button"
-          className="w-full sm:w-[200px] mt-4 sm:mt-[10px] ml-4 sm:ml-[57%] py-2 border rounded-md focus:outline-none focus:border-blue-500 text-white"
-          onClick={handleProjectAdd}
-        >
-          프로젝트 등록하기
-        </button>
+        <div className="text-center pb-[20px]">
+          <button
+            type="button"
+            className="sm:w-[200px] mt-4 sm:mt-[10px] py-2 border rounded-md focus:outline-none focus:border-blue-500 text-white"
+            onClick={handleProjectAdd}
+          >
+            프로젝트 등록하기
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
