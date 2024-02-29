c93fad3184d3d5fc8f074ce3e385d580cd4ab202
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2024-03-01 04:06:45 +0900

    feat#50: 회원가입 정규표현식 수정, 페이지네이션 컴포넌트 구현

--- smuing/src/components/pagination/Pagination.tsx
+++ smuing/src/components/pagination/Pagination.tsx
@@ -1,9 +1,62 @@
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Pagination = ({ totalPage, limit, page, setPage }) => {
-  const [currentPageArray, setCurrentPageArray] = useState([])
-  const [totalPageArray, setTotalPageArray] = useState([])
-  useEffect(()=>{
-    if (page)
-  })
+type PaginationProps = {
+  totalItems: number
+  itemCountPerPage: number
+  pageCount: number
+  currentPage: number
 }
+
+const Pagination: React.FC<PaginationProps> = ({ totalItems, itemCountPerPage, pageCount, currentPage }) => {
+  const totalPages = Math.ceil(totalItems / itemCountPerPage) //
+  const [start, setStart] = useState<number>(1) // 시작 페이지
+  const noPrev = start === 1
+  const noNext = start + pageCount - 1 >= totalPages
+  useEffect(() => {
+    if (currentPage === start + pageCount) {
+      setStart((prev) => prev + pageCount)
+    }
+    if (currentPage < start) {
+      setStart((prev) => prev - pageCount)
+    }
+  }, [currentPage, pageCount, start])
+  return (
+    <div>
+      <ul className="flex list-style-none">
+        <li className={`${noPrev && 'invisible'}`}>
+          <Link
+            className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+            to={`?page=${start - 1}`}
+          >
+            이전
+          </Link>
+        </li>
+        {[...Array(pageCount)].map((_, index) => (
+          <>
+            {start + index <= totalPages && (
+              <li key={index}>
+                <Link
+                  className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+                  to={`?page=${start + index}`}
+                >
+                  {start + index}
+                </Link>
+              </li>
+            )}
+          </>
+        ))}
+        <li className={`${noNext && 'invisible'}`}>
+          <Link
+            className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+            to={`?page=${start + pageCount}`}
+          >
+            다음
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Pagination
--- smuing/src/pages/FormSettingPage.tsx
+++ smuing/src/pages/FormSettingPage.tsx
@@ -1,15 +1,17 @@
 import FormSettingBox from '../components/formsettingbox/FormSettingBox'
+import Pagination from '../components/pagination/Pagination'
 import { FORM_SETTING_DATA } from '../constants/FORM_SETTING_DATA'
 import { FormSettingData } from '../types/types'
 
 const FormSettingPage: React.FC = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-rgb-35-39-49">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-rgb-35-39-49">
       <div className="w-11/12 mt-[120px] py-6 grid lg:grid-cols-4 md:grid-cols-3 gap-4 rounded-xl ">
         {FORM_SETTING_DATA.map((data: FormSettingData) => (
           <FormSettingBox key={data.id} data={data} />
         ))}
       </div>
+      <Pagination totalItems={20} currentPage={1} pageCount={5} itemCountPerPage={10} />
     </div>
   )
 }
