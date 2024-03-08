b782183076755ea48371ecb6e8c66f28559c305c
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-03-08 21:53:10 +0900

    공지사항 무한 스크롤 구현

--- smuing/src/api/notice.ts
+++ smuing/src/api/notice.ts
@@ -1,15 +1,10 @@
 import api from './index'
 
-export const createNotice = async (formData) => {
-  // const requestBody = {
-  //   title: title,
-  //   content: content,
-  //   images: images,
-  // }
+export const createNotice = async (formData: string) => {
   const accessToken = localStorage.getItem('accessToken')
 
   try {
-    const response = await api.post('/api/notice/create', formData, {
+    const response = await api.post('/notice/create', formData, {
       headers: {
         Authorization: accessToken,
         'Content-Type': 'multipart/form-data', // Set the content type to multipart form data
@@ -19,6 +14,27 @@ export const createNotice = async (formData) => {
     return response.data
   } catch (error) {
     console.error(error)
+    throw error
+  }
+}
+
+export const infinityScrollNotice = async (page: number, size: number) => {
+  const accessToken = localStorage.getItem('accessToken')
+  const params = {
+    noticeId: page,
+    size: size,
+  }
+  try {
+    const response = await api.get(`/notice/show`, {
+      params: params,
+      headers: {
+        Authorization: accessToken,
+      },
+    })
+    console.log(response)
+    return response
+  } catch (error) {
+    console.log(error)
     throw error
   }
 }
--- smuing/src/pages/AllAnnounce.tsx
+++ smuing/src/pages/AllAnnounce.tsx
@@ -1,9 +1,59 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import { infinityScrollNotice } from '../api/notice'
 import TextBox from '../components/announce/TextBox'
 import { textData } from '../constants/ANNOUNCE_DATA'
 
 const AllAnnounce = () => {
+  const [page, setPage] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [noticeData, setNoticeData] = useState<string[]>([])
+
+  const infinityLoading = async () => {
+    try {
+      setLoading(true)
+      const response = await infinityScrollNotice(page, 7)
+
+      if (response.data.code == '2000') {
+        const data = response.data.result
+        if (page == 0) {
+          setNoticeData(data)
+        } else {
+          setNoticeData((prevData) => [...prevData, ...data])
+        }
+        console.log(data)
+      } else {
+        console.log('인피니티 에러', response)
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    infinityLoading()
+  }, [page])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!loading) {
+        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+          if (noticeData.length > 0 && !loading) {
+            setPage((prevPage) => prevPage + 1)
+          }
+        }
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [noticeData, loading])
+
   return (
     <div className="bg-rgb-35-39-49 h-full w-full min-h-screen">
       {/* 타이틀 부분 */}
@@ -14,9 +64,15 @@ const AllAnnounce = () => {
 
       {/* 바디 부분 */}
       <div className="flex flex-col items-center mt-[50px]">
-        {textData.map((text, index) => (
+        {noticeData.map((notice, index) => (
           <Link key={index} to={`/announce/1`} className="no-underline">
-            <TextBox key={index} title={text.title} writer={text.writer} date={text.date} bodyText={text.bodyText} />
+            <TextBox
+              key={index}
+              title={notice.title}
+              writer="SMUMC 운영진"
+              date={notice.createdAt}
+              bodyText={notice.content}
+            />
           </Link>
         ))}
       </div>
